22d8915100a71ffe99568549dc62b191c2d96fb3
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2018-08-30 01:41:02 +0900

    [modify] fix layout

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -2,44 +2,41 @@
   <div id="app">
     <div id="main" class="has-text-justified">
       <h1 class="title is-1">Translation</h1>
-      <h4 class="subtitle is-4">テキストボックスの文章をOpenNMTで作成した学習モデルで翻訳した結果を表示します。</h4>
-      <div class="columns">
-        <div class="column">
-          <article class="message">
-            <div class="message-header">
-              <p>原文</p>
-            </div>
-            <div class="message-body">
-              <form v-on:submit.prevent="translate">
-                <textarea class="textarea" v-model="src" name='src' value=''></textarea>
-                <button class="button is-light">
-                  <span class="icon">
-                    <font-awesome-icon prefix="fas" icon="language" />
-                  </span>
-                  <span>翻訳</span>
-                </button>
-              </form>
-            </div>
+      <div class="tile is-ancestor">
+        <div class="tile is-parent">
+          <article class="tile is-child box">
+            <p class="title">原文</p>
+            <p class="subtitle">日本語</p>
+            <form v-on:submit.prevent="translate">
+              <textarea class="content textarea" v-model="src" name='src' value=''></textarea>
+              <button class="button is-light">
+                <span class="icon">
+                  <font-awesome-icon prefix="fas" icon="language" />
+                </span>
+                <span>翻訳</span>
+              </button>
+            </form>
           </article>
         </div>
-        <div class="column">
-          <article class="message">
-            <div class="message-header">
-              <p>訳文</p>
-            </div>
-            <div class="message-body">
-              <div v-for="translations in translationList" :key="translations.id">
-                <div v-for="translation in translations" :key="translation.id">
-              <p>{{ translation.tgt }}</p>
-                </div>
+        <div class="tile is-parent">
+          <article class="tile is-child box">
+            <p class="title">訳文</p>
+            <p class="subtitle">英語</p>
+            <div v-for="translations in translationList" :key="translations.id">
+              <div v-for="translation in translations" :key="translation.id">
+                <p class="content">{{ translation.tgt }}</p>
               </div>
             </div>
           </article>
         </div>
       </div>
-      <a href="https://bulma.io" target="_blank">
-        <img src="https://bulma.io/images/made-with-bulma.png" alt="Made with Bulma" width="128" height="24">
-      </a>
+      <footer class="footer">
+        <div class="content has-text-justfied">
+          <a href="https://bulma.io" target="_blank">
+            <img src="https://bulma.io/images/made-with-bulma.png" alt="Made with Bulma" width="128" height="24">
+          </a>
+        </div>
+      </footer>
     </div>
     <vue-particles
       color="#dedede"
@@ -127,6 +124,7 @@ body,
   top: 50%;
   left: 50%;
   transform: translateY(-50%) translateX(-50%);
+  width: 99%;
 }
 
 h1 {
@@ -134,9 +132,8 @@ h1 {
   text-shadow: 5px 5px 0px #000000;
   font-family: "Dosis", sans-serif;
 }
-h4 {
-  color: #ffffff !important;
-  text-shadow: 5px 5px 0px #000000;
-  font-family: "Dosis", sans-serif;
+
+footer {
+  background-color: transparent !important;
 }
 </style>
